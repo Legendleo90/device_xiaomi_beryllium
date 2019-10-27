53fc950db3992c7e5c437c9c1000900693ed85e4
Ethan Chen <intervigil@gmail.com>
2019-10-26 19:37:49 -0700

    beryllium: Enable Pixel atrace HAL
    Change-Id: I34691e5e909b6ca76dc97fd5c45328e3f80b67e1
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -437,6 +437,10 @@ PRODUCT_COPY_FILES += \
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
 
+# Tracing
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service.pixel
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
