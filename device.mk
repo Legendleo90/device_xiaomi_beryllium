d240e8163a28c89476def189977fdc2fc0392290
Akshat <Legendleo90@users.noreply.github.com>
2024-06-25 14:23:53 +0530

    beryllium: Track hardware/xiaomi - Partially revert https://github.com/Legendleo90/device_xiaomi_beryllium/commit/73f416cabd2059beb9ac537dc2e81c2d85645635

--- device.mk
+++ device.mk
@@ -398,6 +398,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
+    hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
 # Speed profile services and wifi-service to reduce RAM and storage.
